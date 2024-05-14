3bccfc0ae2d9ee4d6cc019b468a96bde38dc1230
Flipps12 <paginasypruebas12@gmail.com>
2024-05-14 12:47:01 -0300

    pruebas admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,6 +3,7 @@ import { getWhitelist } from '../database/controller.js'
 import { authMiddleware } from './sesion.routes.js';
 import { viewWhiteList } from '../rcon/connection.js';
 import { viewPlansDB } from '../database/database.js';
+import { process_webhook } from '../database/controller.js';
 import path from 'path';
 
 const router = Router();
@@ -19,11 +20,20 @@ router.get('/admin', authMiddleware, async (req, res) => {
         res.send('No sos un admin, raja de aca');
         return
     };
-    res.send(await viewWhiteList() + await viewPlansDB())
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'admin.html'));
+});
+
+router.post('/admin/adduser', authMiddleware, async (req, res) => {
+    console.log(req.body)
+    if (!req.user.isAdmin) {
+        res.send('No sos un admin, raja de aca');
+        return
+    };
+    res.send(await process_webhook({ external_reference: [req.body.user, 'Basic server']}))
 });
 
 // ! post() ejecutar comandos en el servidor como administrador (Seguridad alta)
-
+// res.send(await viewWhiteList() + await viewPlansDB())
 // ! get() Mirar lista de jugfadores conectados, en whitelist, pagos, etc
 
 export default router
